9f97e0a1893640aa76183107ae58a6f333c264a2
dmytro <dmytro.solonina@gmail.com>
2013-10-12 18:40:21 +0300

    changed footer

--- app/overrides/footer.rb
+++ app/overrides/footer.rb
@@ -1,1 +1,1 @@
-Deface::Override.new(:virtual_path => "spree/shared/_footer", :name => "footer", :replace_contents => "#footer-left", :text => "<a href='mailto:usolonina@mail.ru'>Yulia Solonina</a> +38(063)355-75-18")
+Deface::Override.new(:virtual_path => "spree/shared/_footer", :name => "footer", :replace_contents => "#footer-left", :text => "<a href='mailto:usolonina@mail.ru'>Юлия Солонина</a> +38 (063) 355-75-18")
