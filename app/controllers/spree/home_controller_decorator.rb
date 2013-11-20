388a81cdcb72d738540b256439499a520e73c0cf
dmytro <dmytro.solonina@gmail.com>
2013-11-20 03:32:30 +0200

    changed taxon searching

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,12 +1,12 @@
 Spree::HomeController.class_eval do
   def index
-    slider = Spree::Taxon.where(:permalink => 'Slider').first
+    slider = Spree::Taxon.where(:permalink => 'slider').first
     @slider_products = slider.products.active if slider
 
-    featured = Spree::Taxon.where(:permalink => 'Featured').first
+    featured = Spree::Taxon.where(:permalink => 'featured').first
     @featured_products = featured.products.active if featured
 
-    latest = Spree::Taxon.where(:permalink => 'Latest').first
+    latest = Spree::Taxon.where(:permalink => 'latest').first
     @latest_products = latest.products.active if latest
   end
 end
